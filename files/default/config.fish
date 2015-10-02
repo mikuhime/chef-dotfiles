1211afd6e1c3a9a5a33b5b3680d41fa1c580fd9a
mikuhime <miku@miku.io>
2015-10-03 00:28:03 +0200

    Fix fish+tmux+powerline

---
+++ files/default/config.fish
@@ -0,0 +1,3 @@
+set -x PATH $PATH /usr/local/bin
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
--- recipes/fish.rb
+++ recipes/fish.rb
@@ -1,6 +1,17 @@
 package 'fish' do
 end
 
+directory '/Users/miku/.config/fish' do
+  owner 'miku'
+  mode '0700'
+end
+
+cookbook_file '/Users/miku/.config/fish/config.fish' do
+  source 'config.fish'
+  mode '0700'
+  owner 'miku'
+end
+
 cookbook_file '/etc/shells' do
   source 'etc_shells'
   user 'root'
