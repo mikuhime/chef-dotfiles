8556a7f1acc26b94cb9f579f94cf29a4a5089dff
mikuhime <miku@miku.io>
2015-10-17 13:04:15 +0200

    Add go config

--- files/default/config.fish
+++ files/default/config.fish
@@ -1,3 +1,6 @@
 set -x PATH $PATH /usr/local/bin
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
+
+set -x GOPATH $HOME/src/go
+set -x PATH $PATH $GOPATH/bin
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-%w(bin src bin/iso).each do |dir|
+%w(bin bin/iso src src/go src/go/bin src/go/src).each do |dir|
   directory "/Users/miku/#{dir}" do
     user 'miku'
     mode '700'
